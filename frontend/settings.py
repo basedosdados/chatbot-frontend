f57a1364cf8e20d67f15d8c27bb3ba29aaad6135
vrtornisiello <vrtornisiello@gmail.com>
2026-04-24 10:16:13 -0300

    chore: update `BDPRO_URL` setting description

--- frontend/settings.py
+++ frontend/settings.py
@@ -26,7 +26,7 @@ class Settings(BaseSettings):
         return f"http://{self.CHATBOT_HOST}:{self.CHATBOT_PORT}"
 
     # BD Pro URL settings
-    BDPRO_URL: NonEmptyStr = Field(description="BD Pro landing page URL")
+    BDPRO_URL: NonEmptyStr = Field(description="BD Pro page URL")
 
     # Logging settings
     LOG_LEVEL: str = Field(
